6b953e0d0d0beafc8645327931db045e5482c60e
nKiNk <39448774+nKiNk@users.noreply.github.com>
2020-12-28 16:15:19 +0900

    Update comparison_nkang.js

--- 1_Get_Started/practice/comparison_nkang.js
+++ 1_Get_Started/practice/comparison_nkang.js
@@ -3,17 +3,41 @@ const dayEnd = "17:45";
 
 function scheduleMeeting(startTime, durationMinutes) {
 	var [ , hour, minutes] = startTime.match(/^(\d{1,2}):(\d{2})$/) || [];
-	if (endMinutes = durationMinutes + minutes >= 60)
+	hour = Number(hour);
+	minutes = Number(minutes);
+	console.log(hour);
+	console.log(minutes);
+	var endMinutes = durationMinutes + minutes;
+	if (endMinutes >= 60)
 	 {
-		 endHour = hour + Math.floor(endMinutes / 60);
-		 endMinutes = endMinutes % 60;
+		var endHour = hour + Math.floor(endMinutes / 60);
+		endMinutes = endMinutes % 60;
 	 }
+	else
+	{
+		var endHour = hour;
+	}
 	var endNumber = endHour * 100 + endMinutes;
-	var dayStartNum = Number(dayStart.replace(":", dayStart);
-	var dayEndNum = Number(dayEnd.replace(":", dayEnd);
+	console.log(endHour);
+	console.log(endMinutes);
+	console.log(endNumber);
+	console.log(dayStart.replace(":", ""));
+	console.log(dayEnd.replace(":", ""));
+	var dayStartNum = Number(dayStart.replace(":", ""));
+	var dayEndNum = Number(dayEnd.replace(":", ""));
+	console.log(dayStartNum);
+	console.log(dayEndNum);
 	if (dayStart < endNumber && dayEnd > endNumber)
 	{
 			return (true);
 	}
 	return (false);
 }
+
+console.log(scheduleMeeting("7:00",15));     // false
+console.log(scheduleMeeting("07:15",30));    // false
+console.log(scheduleMeeting("7:30",30));     // true
+console.log(scheduleMeeting("11:30",60));    // true
+console.log(scheduleMeeting("17:00",45));    // true
+console.log(scheduleMeeting("17:30",30));    // false
+console.log(scheduleMeeting("18:00",15));    // false
