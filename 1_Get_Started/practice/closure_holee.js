0438c72421cd8c826f7ea3ab49f480566a40ff9b
hochan222 <hochan049@gmail.com>
2020-12-29 14:47:12 +0900

    [FEAT] add closure practice, Chapter 1

---
+++ 1_Get_Started/practice/closure_holee.js
@@ -0,0 +1,33 @@
+function range(start,end) { 
+    start = Number(start) || 0;
+  
+    if (end === undefined) {
+      return function getEnd(end) {
+        return getRange(start,end); 
+      };
+    } else {
+      end = Number(end) || 0;
+      return getRange(start,end); 
+    }
+   
+    function getRange(start,end) { 
+      var ret = [];
+  
+      for (let i = start; i <= end; i++) { 
+        ret.push(i);
+      }
+      return ret; 
+    }
+}
+
+range(3,3);  // [3]
+range(3,8);  // [3,4,5,6,7,8]
+range(3,0);  // []
+
+var start3 = range(3); 
+var start4 = range(4);
+
+start3(3);  // [3]
+start3(8);  // [3,4,5,6,7,8]
+start3(0);  // []
+start4(6);  // [4,5,6]
