7fcabc64e78b4a1e258c5404ffa114924b36d1c8
임광열 <mkitigy@gmail.com>
2021-01-12 16:29:36 +0900

    [FEAT] Add closure part 3 practice

---
+++ 2_Scope_Closures/practice_new/closure_part3_gim.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const INPUT_LENGTH = 1;
+const INPUT_INVALID_MSG = 'this is invalid input.';
+const BASE = 10;
+
+function calculator() {
+  const operators = ['+', '-', '*', '/', '='];
+  let operator;
+  let number = 0;
+  let total = 0;
+
+  return calculate;
+
+  function calculate(input) {
+    if (!isValidInput(input)) {
+      throw new Error(INPUT_INVALID_MSG);
+    }
+    if (operators.includes(input) && operate()) {
+      operator = input;
+      number = 0;
+
+      return operator === '=' ? formatTotal(total) : input;
+    }
+    if (operator === '=') {
+      operator = null;
+      total = 0;
+    }
+    number = operator === '=' ? Number(input) : number * BASE + Number(input);
+
+    return input;
+  }
+
+  function isValidInput(input) {
+    if (input.length !== INPUT_LENGTH) {
+      return false;
+    }
+    if (isNaN(Number(input)) && !operators.includes(input)) {
+      return false;
+    }
+
+    return true;
+  }
+
+  function operate() {
+    if (!operator) {
+      total = number;
+    } else if (operator === '+') {
+      total += number;
+    } else if (operator === '-') {
+      total -= number;
+    } else if (operator === '*') {
+      total *= number;
+    } else if (operator === '/') {
+      total /= number;
+    }
+
+    return true;
+  }
+}
