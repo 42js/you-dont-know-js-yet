987b43bc9a2d9d8f3f6ea24520d2a9d8f3bdaaf7
hochan222 <hochan049@gmail.com>
2021-01-13 16:28:44 +0900

    [FEAT] add prime cache

--- 2_Scope_Closures/practice_new/closure_part1_holee.js
+++ 2_Scope_Closures/practice_new/closure_part1_holee.js
@@ -1,24 +1,38 @@
-function isPrime(v) { 
-    if (v <= 3) {
-        return v > 1; 
-    }
-    if (v % 2 === 0 || v % 3 === 0) { 
-        return false;
+const isPrime = (function () {
+    const cache = {};
+
+    return (checkPrimes);
+
+    function checkPrimes(v) {
+        if (v in cache)
+            return cache[v];
+        return (cache[v] = isPrime(v));
     }
 
-    var vSqrt = Math.sqrt(v);
-    for (let i = 5; i <= vSqrt; i += 6) {
-        if (v % i === 0 || v % (i + 2) === 0) { 
+    function isPrime(v) { 
+        if (v <= 3) {
+            return v > 1; 
+        }
+        if (v % 2 === 0 || v % 3 === 0) { 
             return false;
-        } 
+        }
+
+        var vSqrt = Math.sqrt(v);
+        for (let i = 5; i <= vSqrt; i += 6) {
+            if (v % i === 0 || v % (i + 2) === 0) { 
+                return false;
+            } 
+        }
+        return true; 
     }
-    return true; 
-}
+})();
 
 var factorize = (function (_) {
     var cache = {};
     
-    var factorize_algorithm = function (v) { 
+    return (factorize_algorithm);
+
+    function factorize_algorithm(v) {
         if (cache[v.toString()])
             return [v];
         if (!isPrime(v)) {
@@ -32,10 +46,8 @@ var factorize = (function (_) {
             ];
         }
         cache[v.toString()] = true;
-        return [v]; 
+        return [v];
     }
-    
-    return (factorize_algorithm);
 })();
 
 console.log(isPrime(11));
