33cad9010151963b31f577dc3ebac48938c14ae1
Sungwoo Park <codest99@gmail.com>
2021-01-14 00:16:42 +0900

    [FIX]: change tab to two space

--- 2_Scope_Closures/practice_new/closure_part3_sunpark.js
+++ 2_Scope_Closures/practice_new/closure_part3_sunpark.js
@@ -1,97 +1,97 @@
 function calculator() {
-	var isResult = false;
-	var saveNum = 0;
-	var saveIn = 0;
-	var saveOp = "";
+  var isResult = false;
+  var saveNum = 0;
+  var saveIn = 0;
+  var saveOp = "";
 
-	return function displayCurr(input) {
-		if (isNaN(input)) {
-			if (saveOp === "") {
-				if (input === "=") {
-					isResult = true;
-					return formatTotal(saveNum);
-				}
-				saveOp = input;
-				isResult = false;
-			}
-			else {
-				switch (saveOp) {
-					case "+":
-						saveNum = saveNum + saveIn;
-						break;
-					case "-":
-						saveNum = saveNum - saveIn;
-						break;
-					case "*":
-						saveNum = saveNum * saveIn;
-						break;
-					case "/":
-						saveNum = saveNum / saveIn;
-						break;
-					default:
-						break;
-				}
-				saveIn = 0;
-				if (input === "=") {
-					saveOp = ""
-					isResult = true;
-					return formatTotal(saveNum);
-				}
-				saveOp = input;
-			}
-		} else {
-			if (saveOp === "") {
-				if (isResult)
-					saveNum = 0;
-				saveNum = saveNum * 10 + parseInt(input);
-				isResult = false;
-			}
-			else
-				saveIn = saveIn * 10 + parseInt(input);
-		}
-		return input;
-	}
+  return function displayCurr(input) {
+    if (isNaN(input)) {
+      if (saveOp === "") {
+        if (input === "=") {
+          isResult = true;
+          return formatTotal(saveNum);
+        }
+        saveOp = input;
+        isResult = false;
+      }
+      else {
+        switch (saveOp) {
+          case "+":
+            saveNum = saveNum + saveIn;
+            break;
+          case "-":
+            saveNum = saveNum - saveIn;
+            break;
+          case "*":
+            saveNum = saveNum * saveIn;
+            break;
+          case "/":
+            saveNum = saveNum / saveIn;
+            break;
+          default:
+            break;
+        }
+        saveIn = 0;
+        if (input === "=") {
+          saveOp = ""
+          isResult = true;
+          return formatTotal(saveNum);
+        }
+        saveOp = input;
+      }
+    } else {
+      if (saveOp === "") {
+        if (isResult)
+          saveNum = 0;
+        saveNum = saveNum * 10 + parseInt(input);
+        isResult = false;
+      }
+      else
+        saveIn = saveIn * 10 + parseInt(input);
+    }
+    return input;
+  }
 
-	function formatTotal(display) {
-		if (Number.isFinite(display)) {
-			// constrain display to max 11 chars
-			let maxDigits = 11;
-			// reserve space for "e+" notation?
-			if (Math.abs(display) > 99999999999) {
-				maxDigits -= 6;
-			}
-			// reserve space for "-"?
-			if (display < 0) {
-				maxDigits--;
-			}
+  function formatTotal(display) {
+    if (Number.isFinite(display)) {
+      // constrain display to max 11 chars
+      let maxDigits = 11;
+      // reserve space for "e+" notation?
+      if (Math.abs(display) > 99999999999) {
+        maxDigits -= 6;
+      }
+      // reserve space for "-"?
+      if (display < 0) {
+        maxDigits--;
+      }
 
-			// whole number?
-			if (Number.isInteger(display)) {
-				display = display
-					.toPrecision(maxDigits)
-					.replace(/\.0+$/, "");
-			}
-			// decimal
-			else {
-				// reserve space for "."
-				maxDigits--;
-				// reserve space for leading "0"?
-				if (
-					Math.abs(display) >= 0 &&
-					Math.abs(display) < 1
-				) {
-					maxDigits--;
-				}
-				display = display
-					.toPrecision(maxDigits)
-					.replace(/0+$/, "");
-			}
-		}
-		else {
-			display = "ERR";
-		}
-		return display;
-	}
+      // whole number?
+      if (Number.isInteger(display)) {
+        display = display
+          .toPrecision(maxDigits)
+          .replace(/\.0+$/, "");
+      }
+      // decimal
+      else {
+        // reserve space for "."
+        maxDigits--;
+        // reserve space for leading "0"?
+        if (
+          Math.abs(display) >= 0 &&
+          Math.abs(display) < 1
+        ) {
+          maxDigits--;
+        }
+        display = display
+          .toPrecision(maxDigits)
+          .replace(/0+$/, "");
+      }
+    }
+    else {
+      display = "ERR";
+    }
+    return display;
+  }
 }
 
 var calc = calculator();
@@ -112,21 +112,21 @@ console.log(calc("5"));     // 5
 console.log(calc("="));     // 0
 
 function useCalc(calc, keys) {
-	return [...keys].reduce(
-		function showDisplay(display, key) {
-			var ret = String(calc(key));
-			return (
-				display +
-				(
-					(ret != "" && key == "=") ?
-						"=" :
-						""
-				) +
-				ret
-			);
-		},
-		""
-	);
+  return [...keys].reduce(
+    function showDisplay(display, key) {
+      var ret = String(calc(key));
+      return (
+        display +
+        (
+          (ret != "" && key == "=") ?
+            "=" :
+            ""
+        ) +
+        ret
+      );
+    },
+    ""
+  );
 }
 
 console.log(useCalc(calc, "4+3="));           // 4+3=7
